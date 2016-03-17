cbb2bf03ea1e57ea604feed8ca83989d12c4952f
ChadClark_Dev <chadclarke@bell.net>
2016-03-17 09:05:01 -0400

    first commit

---
+++ Scripts/app.js
@@ -0,0 +1,34 @@
+"use strict";
+
+// setup your IIFE (Immediately Invoked Function Expression)
+(function(){
+   
+  console.log("App Started..."); 
+    
+    
+    //named function way 
+  //   function replaceFirstParagraph(){
+         
+    //    var firstParagraph; 
+        
+      //  firstParagraph = document.getElementById("firstParagraph");
+        
+      //  firstParagraph.innerHTML = "MY New Paragraph Data"
+   
+  //   }
+    // call the replaced first paragrapgh function 
+   // replaceFirstParagraph(); 
+    
+    
+    
+     var firstParagraph; 
+    
+    firstParagraph = document.getElementById("firstParagraph");
+    
+    firstParagraph.innerHTML = "MY New Paragraph Data"
+
+    
+    
+    
+    
+})();
