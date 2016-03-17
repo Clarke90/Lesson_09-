591ebccf1836717778671716d109a2adb4f8dc67
ChadClark_Dev <chadclarke@bell.net>
2016-03-17 09:42:09 -0400

    more updates

--- Scripts/app.js
+++ Scripts/app.js
@@ -10,9 +10,20 @@
     request.open('GET', '../person.txt', true);
     request.addEventListener('readystatechange', function() {
         if (request.readyState === 4) {
+            // declare addres boook array 
              var addressBook = []; /* var Person = new Object();   */
             
+            // read in data from the person.txt file 
              addressBook = JSON.parse(request.responseText);
+             
+           var addressBookLength = addressBook.length; 
+        //   for each person in address boook loop 
+           for(var person in address){
+            //    assign say hello method to each person objest 
+               Person.sayHello = function() {
+               output += "<br><hr><br>" + Person.name + " says hello";
+               
+           } 
             
             // var Person = {}; /* var Person = new Object();   */
 
@@ -21,7 +32,7 @@
             // Person.sayHello = function() {
             //     output += "<br><hr><br>" + Person.name + " says hello";
             }
-
+            console.log(addressBook);
             // for every key in the Person object, loop...
         //     for (var key in Person) {
 
